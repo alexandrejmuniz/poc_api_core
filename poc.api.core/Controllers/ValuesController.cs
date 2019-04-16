a58914dd7010c743bbfc7f4f6fc15e85e11953bc
Alexandre Muniz <alexandrejmuniz@gmail.com>
2019-04-16 17:38:09 -0300

    Rename de métodos

--- poc.api.core/Controllers/ValuesController.cs
+++ poc.api.core/Controllers/ValuesController.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 
-namespace poc.api.core.Controllers
+namespace WebInterface.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
